1afd8a09726fb42c1f227aba33e40380aa6ee0e0
Alex sefer <alexsefer657@gmail.com>
2026-08-29 01:57:25 +0530

    Add alarm broadcast receiver

---
+++ app/src/main/java/com/ayushdebbarma/myaiagent/AlarmReceiver.java
@@ -0,0 +1,9 @@
+package com.ayushdebbarma.myaiagent;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {}
+}
